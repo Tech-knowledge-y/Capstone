2dcb9a12adbefe6b458851c55fa5613b2c7be6b2
Jillian Wysoki <jillianwysoki@gmail.com>
2018-09-16 16:16:32 -0500

    uploading capstone sandbox

--- src/main/java/com/capstone/sandbox/Controllers/HomeController.java
+++ src/main/java/com/capstone/sandbox/Controllers/HomeController.java
@@ -1,4 +1,19 @@
 package com.capstone.sandbox.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/home")
+        public String home() {
+            return "developers";
+    }
+
 }
