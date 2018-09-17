fca441a03a3e45c19d8f087b901b70c53917e0ae
Jillian Wysoki <jillianwysoki@gmail.com>
2018-09-17 12:06:07 -0500

    configuring files

--- src/main/java/com/capstone/tech/controllers/HomeController.java
+++ src/main/java/com/capstone/tech/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.capstone.sandbox.Controllers;
+package com.capstone.tech.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
