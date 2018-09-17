3a6354c982fdaf99a452f4df60ebbd36e7a7c41f
Amos Davis <amos.davis367@gmail.com>
2018-09-17 16:59:32 -0500

    controller connection from edit profile page to view all profiles page complete

---
+++ src/main/java/com/capstone/tech/controllers/UserDetailsController.java
@@ -0,0 +1,42 @@
+package com.capstone.tech.controllers;
+
+import com.capstone.tech.models.UserDetails;
+import com.capstone.tech.repositories.UserDetailsRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserDetailsController {
+    UserDetailsRepo userDetailsdao;
+
+    public UserDetailsController(UserDetailsRepo userDetailsdao) {
+
+        this.userDetailsdao = userDetailsdao;
+    }
+
+
+
+
+
+    @GetMapping("/editProfile")
+    public String showEditProfileForm() {
+        return "users/editProfileForm";
+    }
+
+    @PostMapping("/editProfile")
+    public String updateProfile(@ModelAttribute UserDetails userDetails) {
+//        userDetailsdao.save(userDetails);
+        return "users/all-users";
+    }
+
+//    @PostMapping("/posts/delete")
+//    public String deleteUserDetails(@RequestParam(firstName = "id") long id){
+//        userDetailsdao.deleteUserDetailsByLNameMatches()
+////        return "redirect:/posts";
+//    }
+
+
+
+}
