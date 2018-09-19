a6c1f489c646bfb77752cfe5a7ef1d1bc36cc066
Graham Davis <davis.graham.satx@gmail.com>
2018-09-19 12:13:55 -0500

    added login and signup forms and functionality

--- src/main/java/com/capstone/tech/controllers/UserController.java
+++ src/main/java/com/capstone/tech/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.capstone.tech.controllers;
 
+import com.capstone.tech.models.User;
 import com.capstone.tech.repositories.UserRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -14,6 +17,19 @@ public class UserController {
 
     public UserController(UserRepo userDao) {
         this.userDao = userDao;
+    }
+
+    // User Registration
+    @GetMapping("/register")
+    private String showRegisterForm(Model viewModel){
+        viewModel.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    private String saveUser(@ModelAttribute User user){
+        userDao.save(user);
+        return "redirect:/login";
     }
 
     // Show all users
