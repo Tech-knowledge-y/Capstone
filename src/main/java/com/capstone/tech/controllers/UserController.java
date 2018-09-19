ca3e246ec6ac46e7995b1f92ba68d9b257e24c58
Amos Davis <amos.davis367@gmail.com>
2018-09-19 14:06:55 -0500

    pushing branch to find 405 error

--- src/main/java/com/capstone/tech/controllers/UserController.java
+++ src/main/java/com/capstone/tech/controllers/UserController.java
@@ -1,20 +1,42 @@
 package com.capstone.tech.controllers;
 
+import com.capstone.tech.models.User;
 import com.capstone.tech.repositories.UserRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class UserController {
 
     UserRepo userDao;
+    private PasswordEncoder passwordEncoder;
 
 
-    public UserController(UserRepo userDao) {
+
+    public UserController(UserRepo userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // User Registration
+    @GetMapping("/register")
+    private String showRegisterForm(Model viewModel){
+        viewModel.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    private String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
     }
 
     // Show all users
