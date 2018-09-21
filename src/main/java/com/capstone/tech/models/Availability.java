849d8bcd97f3037e754a6c7cb73b9e6c1137e277
Graham Davis <davis.graham.satx@gmail.com>
2018-09-21 15:33:05 -0500

    added availabiity to user details - not feature complete yet

--- src/main/java/com/capstone/tech/models/Availability.java
+++ src/main/java/com/capstone/tech/models/Availability.java
@@ -3,7 +3,6 @@ package com.capstone.tech.models;
 import javax.persistence.*;
 import java.sql.Time;
 
-
 @Entity
 @Table(name = "availability")
 public class Availability {
@@ -12,14 +11,20 @@ public class Availability {
     @GeneratedValue
     private long id;
 
-    @Column()
-    private String day_of_week;
+    @Column
+    private String dayOfWeek;
 
-    @Column()
-    private String time_start;
+    @Column
+    private String timeStart;
 
-    @Column()
-    private String time_end;
+    @Column
+    private String timeEnd;
+
+    @Column
+    private String week;
+
+    @Column
+    private String year;
 
     // relationships
 
@@ -28,23 +33,29 @@ public class Availability {
 
     // constructors/getters/setters
 
-    public Availability() { }
+    public Availability() {
 
-    public Availability(String day_of_week, String time_start, String time_end, User user) {
-        this.day_of_week = day_of_week;
-        this.time_start = time_start;
-        this.time_end = time_end;
+    }
+
+
+    public Availability(String dayOfWeek, String timeStart, String timeEnd, String week, String year, User user) {
+        this.dayOfWeek = dayOfWeek;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.week = week;
+        this.year = year;
         this.user = user;
     }
 
-    public Availability(long id, String day_of_week, String time_start, String time_end, User user) {
+    public Availability(long id, String dayOfWeek, String timeStart, String timeEnd, String week, String year, User user) {
         this.id = id;
-        this.day_of_week = day_of_week;
-        this.time_start = time_start;
-        this.time_end = time_end;
+        this.dayOfWeek = dayOfWeek;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.week = week;
+        this.year = year;
         this.user = user;
     }
-
 
     public long getId() {
         return id;
@@ -54,28 +65,44 @@ public class Availability {
         this.id = id;
     }
 
-    public String getDay_of_week() {
-        return day_of_week;
+    public String getDayOfWeek() {
+        return dayOfWeek;
     }
 
-    public void setDay_of_week(String day_of_week) {
-        this.day_of_week = day_of_week;
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
     }
 
-    public String getTime_start() {
-        return time_start;
+    public String getTimeStart() {
+        return timeStart;
     }
 
-    public void setTime_start(String time_start) {
-        this.time_start = time_start;
+    public void setTimeStart(String timeStart) {
+        this.timeStart = timeStart;
     }
 
-    public String getTime_end() {
-        return time_end;
+    public String getTimeEnd() {
+        return timeEnd;
     }
 
-    public void setTime_end(String time_end) {
-        this.time_end = time_end;
+    public void setTimeEnd(String timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
     }
 
     public User getUser() {
