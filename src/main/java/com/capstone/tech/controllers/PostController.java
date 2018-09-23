51eae7515b23e710e460ca24c186934c13b9ac68
Xavier Salazar <salazar.xavier26@gmail.com>
2018-09-22 22:35:44 -0500

    commenting on single post feature

--- src/main/java/com/capstone/tech/controllers/PostController.java
+++ src/main/java/com/capstone/tech/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.capstone.tech.controllers;
 
+import com.capstone.tech.models.Comments;
 import com.capstone.tech.models.Post;
+import com.capstone.tech.repositories.CommentsRepo;
 import com.capstone.tech.repositories.PostRepo;
 import com.capstone.tech.repositories.UserRepo;
 import com.capstone.tech.services.UserService;
@@ -8,16 +10,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class PostController {
 
     PostRepo postDao;
     UserRepo userDao;
+    CommentsRepo commentsDao;
     UserService userSvc;
 
-    private PostController(PostRepo postDao, UserRepo userDao, UserService userSvc) {
+    private PostController(PostRepo postDao, UserRepo userDao, CommentsRepo commentsDao, UserService userSvc) {
         this.postDao = postDao;
         this.userDao = userDao;
+        this.commentsDao = commentsDao;
         this.userSvc = userSvc;
     }
 
@@ -34,10 +40,21 @@ public class PostController {
     @GetMapping("/posts/{id}")
     private String show(@PathVariable long id, Model model) {
         model.addAttribute("post", postDao.findOne(id));
+        model.addAttribute("comment", new Comments());
+        model.addAttribute("allComments", commentsDao.findAll());
         if(postDao.findOne(id).getUser()==userSvc.loggedInUser()){
             model.addAttribute("isOwner", true);
         }
         return "posts/show";
+    }
+
+    // Commenting
+    @PostMapping("/posts/{pId}/{cId}")
+    public String commenting(@PathVariable long pId, @ModelAttribute Post post, @ModelAttribute Comments comments) {
+        comments.setUser(userSvc.loggedInUser());
+        comments.setPost(postDao.findOne(pId));
+        commentsDao.save(comments);
+        return "redirect:/posts/" + pId;
     }
 
     @GetMapping("/posts/create")
