235c187af40a0cfcaa050034c5dedf74424f0660
Xavier Salazar <salazar.xavier26@gmail.com>
2018-10-01 16:14:31 -0500

    xs-login-signup

--- src/main/java/com/capstone/tech/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/tech/controllers/AuthenticationController.java
@@ -1,12 +1,15 @@
 package com.capstone.tech.controllers;
 
+import com.capstone.tech.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model viewModel) {
+        viewModel.addAttribute("user", new User());
         return "users/login";
     }
 }
